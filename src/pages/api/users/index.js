2a9ae9ec4b07a56b005afb1a456507495b3c6172
Արսեն Գաբրիելյան / Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2023-10-27 19:09:31 +0400

    Another Minor Change

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -42,7 +42,7 @@ export default async function handler(req,res){
                          'comments.$[i].name': updatedUser?.username,
                          'comments.$[i].image': updatedUser?.image
                     }
-               },{arrayFilters: [{'i.email': {$eq: updatedUser?.email || accSettings?.email}}]})
+               },{arrayFilters: [{'i.email': {$eq: accSettings?.email}}]})
                res.status(200).json(updatedUser);
           }
           else res.status(400).json({msg: 'Failed To Update'})
