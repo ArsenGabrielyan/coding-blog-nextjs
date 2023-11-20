72c7b10baf7dcf75831b41b472b35731d865fab9
Արսեն Գաբրիելյան / Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2023-11-20 17:08:21 +0400

    A Small Changes (Changing Colors on some scss files)

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -3,7 +3,7 @@ import {createTransport} from "nodemailer";
 const USER = process.env.BASE_EMAIL;
 const getHtml = (link,user) =>`<h1>Reset Your Password</h1>
 <p>Hey ${user.name.split(' ')[0]}! Seems Like You Forgot Your Password. If It is True, Click on the Link Below To Reset Your Password</p>
-<a href="${link}" style="font-size: 21px; color:#167051">Reset Password</a>
+<a href="${link}" style="font-size: 21px; color:#005999">Reset Password</a>
 <p>If You Didn't Forget Your Password, You can Simply Ignore This Message.</p>`;
 export const sendEmail = async(user,email,subject,text)=>{
      const transporter = createTransport({
